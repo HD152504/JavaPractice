2d8d3b396f35454a4b35cc988b09de53e0d7b2d1
HD152504 <rlaskagus301@naver.com>
2017-03-15 14:53:23 +0900

    commit

---
+++ src/org/dimigo/basic/new1.java
@@ -0,0 +1,17 @@
+package org.dimigo.basic;
+
+/**
+ * <pre>
+ *  org.dimigo.basic
+ *     |_ new1
+ * 1. 개요 :
+ * 2. 작성일 : 2017-03-14
+ * <pre>
+ * @author : lg
+ * @version : 1.0
+ */
+public class new1 {
+    public static void main(String[] args) {
+
+    }
+}
