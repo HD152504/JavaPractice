8d6acc6756309a5e039e91888393d2e32de7b011
HD152504 <rlaskagus301@naver.com>
2017-05-12 14:40:56 +0900

    commit

---
+++ src/org/dimigo/inheritance/SmartPhone.java
@@ -0,0 +1,47 @@
+package org.dimigo.inheritance;
+
+/**
+ * <pre>
+ *  org.dimigo.inheritance
+ *     |_ SmartPhone
+ * 1. 개요 :
+ * 2. 작성일 : 2017-05-12
+ * <pre>
+ * @author : lg
+ * @version : 1.0
+ */
+public class SmartPhone {
+    private String model;
+    private String company;
+    private int price;
+
+    public SmartPhone() {}
+
+    public SmartPhone(String model, String company, int price) {
+        this.model = model;
+        this.company = company;
+        this.price = price;
+    }
+    public void turnOn() {
+        System.out.println(model + "의 전원을 켭니다.");
+    }
+    public void turnOff() {
+        System.out.println(model + "의 전원을 끕니다.");
+    }
+    public void pay() {}
+    public void useSpecialFunction (SmartPhone phone) {
+        if (phone == null) {
+            System.out.println("null값 넣지마세요...");
+            return;
+        }
+        if (phone instanceof IPhone) {
+            ((IPhone) phone).useAirDrop();
+        } else if (phone instanceof Galaxy) {
+            ((Galaxy) phone).useWirelessCharging();
+        } else return;
+    }
+    public String toString() {
+        return "모델명 : " + model + ", 제조사 : " + company + ", 가격 : " + String.format("%,d", price) + "원";
+    }
+
+}
---
+++ src/org/dimigo/inheritance/SmartPhoneTest.java
@@ -0,0 +1,32 @@
+package org.dimigo.inheritance;
+
+/**
+ * <pre>
+ *  org.dimigo.inheritance
+ *     |_ SmartPhoneTest
+ * 1. 개요 :
+ * 2. 작성일 : 2017-05-12
+ * <pre>
+ * @author : lg
+ * @version : 1.0
+ */
+public class SmartPhoneTest {
+    public static void main(String[] args) {
+        SmartPhone[] phone = {
+                new IPhone("iPhone 7", "애플", 900000),
+                new IPhone("Galaxy 8", "삼성", 800000),
+        };
+
+        for (SmartPhone p: phone ) {
+            System.out.println(p);
+            p.turnOn();
+            p.pay();
+            p.useSpecialFunction(p);
+            p.turnOff();
+            System.out.println();
+        }
+
+
+
+    }
+}
