ff4f9e8c24b06cc7649d9c0cc17aefdc7414f6e1
HD152504 <rlaskagus301@naver.com>
2017-05-17 15:35:06 +0900

    commit

---
+++ src/org/dimigo/abstractclass/SmartPhoneTest.java
@@ -0,0 +1,31 @@
+package org.dimigo.abstractclass;
+
+/**
+ * <pre>
+ *  org.dimigo.inheritance
+ *     |_ SmartPhoneTest
+ * 1. 개요 :
+ * 2. 작성일 : 2017-05-12
+ * <pre>
+ * @author : lg
+ * @version : 1.0
+ */
+public class SmartPhoneTest {
+    public static void main(String[] args) {
+        SmartPhone[] phone = {
+                new IPhone("iPhone 7", "애플", 900000),
+                new Galaxy("Galaxy 8", "삼성", 800000),
+        };
+
+        for (SmartPhone p: phone ) {
+            System.out.println(p);
+            p.turnOn();
+            p.pay();
+            p.useSpecialFunction();
+            p.turnOff();
+            System.out.println();
+        }
+
+
+    }
+}
