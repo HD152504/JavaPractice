eaf5d13b00dc0e81600ad957b5ced834d9a63121
HD152504 <rlaskagus301@naver.com>
2017-05-30 12:28:04 +0900

    commit

---
+++ src/org/dimigo/exception/Movie.java
@@ -0,0 +1,38 @@
+package org.dimigo.exception;
+
+/**
+ * <pre>
+ *  org.dimigo.exception
+ *     |_ Movie
+ * 1. 개요 :
+ * 2. 작성일 : 2017-05-29
+ * <pre>
+ * @author : lg
+ * @version : 1.0
+ */
+public class Movie {
+    private String title;
+    private int limitAge;
+
+    public Movie(String title, int limitAge) {
+        this.title = title;
+        this.limitAge = limitAge;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getLimitAge() {
+        return limitAge;
+    }
+
+    public void buyTicket(int age) throws Exception {
+        if(this.getLimitAge() > age) {
+            throw new Exception(this.getTitle() + "은/는 " + this.getLimitAge() + "세 이상 관람가입니다.");
+        } else {
+            System.out.println(this.getTitle() + " 즐감하세요.");
+        }
+    }
+
+}
